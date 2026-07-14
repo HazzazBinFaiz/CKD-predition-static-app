19d9b2c19e8db6426469071253ac74675cca4f7c
MD. Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2026-07-14 18:06:18 +0600

    Use relative base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // If deploying to GitHub Pages at https://user.github.io/REPO/,
-  // uncomment and set base: base: "/REPO/",
+  base: './',
   plugins: [react(), tailwindcss()],
 });
